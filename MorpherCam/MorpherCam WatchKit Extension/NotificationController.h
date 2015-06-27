71b05840be0f37488d0541fccc471214f50f0b47
UDONKONET <dev@udonko.net>
2015-06-28 00:29:35 +0900

    プロジェクト作り直し

---
+++ MorpherCam/MorpherCam WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  MorpherCam WatchKit Extension
+//
+//  Created by UDONKONET on 2015/06/27.
+//  Copyright (c) 2015年 UDONKONET. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
---
+++ MorpherCam/MorpherCam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PowerMorpher
+//
+//  Created by UDONKONET on 2015/05/24.
+//  Copyright (c) 2015年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
